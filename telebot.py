065cf44d81d294e60b2265b3eed1d1a78e159f02
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-02-09 23:02:45 +0300

    publish photo to Telegram channel.

--- telebot.py
+++ telebot.py
@@ -1,4 +1,4 @@
 import telegram
 
 bot = telegram.Bot(token='6249525717:AAEgl00yHNRKsuTiqiLIdNHYC6ZREzlqrJc')
-bot.send_message(text='teletest', chat_id=-1001836409919)
+bot.send_document(chat_id=-1001836409919, document=open('Images/planetary_0.jpg', 'rb'))
