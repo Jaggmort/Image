329616d35e91f8849792627dbb752876d4d16351
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-02-09 23:49:53 +0300

    Add bot whom post images into channel every 4 h.

--- telebot.py
+++ telebot.py
@@ -1,4 +1,18 @@
 import telegram
+import os
+import random
+import time
+from dotenv import load_dotenv
 
-bot = telegram.Bot(token='6249525717:AAEgl00yHNRKsuTiqiLIdNHYC6ZREzlqrJc')
-bot.send_document(chat_id=-1001836409919, document=open('Images/planetary_0.jpg', 'rb'))
+
+if __name__=='__main__':
+    load_dotenv()
+    sleep_timer = os.environ.get('SLEEP_TIMER')    
+    while True:
+        time.sleep(sleep_timer)
+        bot = telegram.Bot(token='6249525717:AAEgl00yHNRKsuTiqiLIdNHYC6ZREzlqrJc')
+        images_list = (os.walk('Images'))
+        img = []
+        for i in images_list:
+            img.append(i[2])
+        bot.send_document(chat_id=-1001836409919, document=open(f'Images/{random.choice(img[0])}', 'rb'))
