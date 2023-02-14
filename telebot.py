7807cb6c277f825ea6e1bca86a98da3f7b6b648e
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-02-14 12:26:58 +0300

    Update telebot.py
    Fixed empy list comparsion

--- telebot.py
+++ telebot.py
@@ -6,14 +6,20 @@ import send_photo
 from dotenv import load_dotenv
 
 
-if __name__=='__main__':
+def main():
     load_dotenv()
     sleep_timer = os.environ.get('SLEEP_TIMER')
+    chat_id = os.environ.get('CHAT_ID')
+    telegram_token = os.environ.get('TELEGRAM_TOKEN')    
     available_images = []
     while True:
         time.sleep(int(sleep_timer))
-        if available_images == [] or available_images[0] == []:
-            available_images = send_photo.get_images_list()
-        random_choice = random.choice(available_images[0])
-        send_photo.send_photo(f'Images/{random_choice}')
-        available_images[0].remove(random_choice)
+        if not available_images:
+            available_images = send_photo.get_images()
+        random_choice = random.choice(available_images)
+        send_photo.send_photo(f'Images/{random_choice}', chat_id, telegram_token)
+        available_images.remove(random_choice)
+
+
+if __name__=='__main__':
+    main()
