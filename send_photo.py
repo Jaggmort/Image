0f461d737f19d3ee6f1e7659f85c181d714f0d13
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-02-14 12:08:19 +0300

    Update send_photo.py
    Moved load_dotenv() into def main()

--- send_photo.py
+++ send_photo.py
@@ -4,6 +4,16 @@ import telegram
 import argparse
 from dotenv import load_dotenv
 
+
+def main():
+    load_dotenv()
+    chat_id = os.environ.get('CHAT_ID')
+    telegram_token = os.environ.get('TELEGRAM_TOKEN')
+    parser=argparse.ArgumentParser(description='Получает изображения запуска spacex по id')
+    parser.add_argument('--Image', help='Имя файла для отправки',default=f'{random.choice(get_images()[0])}')
+    args=parser.parse_args()   
+    send_photo(f'Images/{args.Image}', chat_id=chat_id, telegram_token=telegram_token) 
+
 def get_images():
     images = (os.walk('Images'))
     available_images = []
@@ -11,10 +21,7 @@ def get_images():
         available_images.append(image[2])
     return available_images
 
-def send_photo(Image):
-    load_dotenv()
-    chat_id = os.environ.get('CHAT_ID')
-    telegram_token = os.environ.get('TELEGRAM_TOKEN')
+def send_photo(Image, chat_id, telegram_token):
     bot = telegram.Bot(token=telegram_token)
     with open (Image, 'rb') as document:
         bot.send_document(chat_id=chat_id, document=document)
@@ -22,7 +29,4 @@ def send_photo(Image):
 
 
 if __name__=='__main__':
-    parser=argparse.ArgumentParser(description='Получает изображения запуска spacex по id')
-    parser.add_argument('--Image', help='Имя файла для отправки',default=f'{random.choice(get_images()[0])}')
-    args=parser.parse_args()   
-    send_photo(f'Images/{args.Image}')
+    main()
